ffbb8083aef2fcef4d2c197f05685ea8118708b9
lokeshpalleda <160224924+lokeshpalleda@users.noreply.github.com>
2024-10-22 22:55:58 +0530

    Create p1.java

---
+++ exam/p1.java
@@ -0,0 +1,12 @@
+public class A
+{
+    static void mymethod(String name,int age)
+    {
+        System.out.println(name+age);
+    }
+    public static void main(String []args)
+    {
+        mymethod("arjun",22);
+        mymethod("arjun",22);
+    }
+}
